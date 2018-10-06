0f1296729b0d48de53547d3153ebb56134ebaac3
Jorge Rearte <jorgereartec@gmail.com>
2018-10-06 19:31:04 -0300

    impementacion de ng-select para selccionar un elemento de una lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, NgModule, ViewChild} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormControl, FormGroup, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {NgSelectModule, NgOption} from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba2';
+  cities = [
+        {id: 1, name: 'Vilnius'},
+        {id: 2, name: 'Kaunas'},
+        {id: 3, name: 'Pavilnys', disabled: true},
+        {id: 4, name: 'Pabradė'},
+        {id: 5, name: 'Klaipėda'}
+    ];
+    
+    
+
+    selectedCity: any;
+    selectedCityIds: string[];
+    selectedCityName = 'Vilnius';
+    selectedCityId: number;
+    selectedUserIds: number[];
+    
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, NgSelectModule ],
+  providers: []
 })
 export class AppModule { }
+
